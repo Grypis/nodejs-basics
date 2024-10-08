7b034f5ce5469565a20e79f1bbee1bc650b6e46c
grypis <grypisnecros@gmail.com>
2024-10-09 02:25:20 +0300

    ++

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,11 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
 
 export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  {
-    languageOptions: { globals: globals.node },
-    rules: {
-      semi: 'error',
-      'no-unused-vars': 'error',
-      'no-undef': 'error',
-    },
-  },
+  
 ];
---
+++ lesson-2/01-path/movies/index.js
@@ -0,0 +1,13 @@
+import path from "node:path";
+import * as fs from 'node:fs/promises';
+// async function readMovies() {
+//     const data = await fs.readFile("movies.txt", { encoding: "utf8" });
+
+//     return data;
+// }
+
+export function readFile() {
+    const FILE_PATH = path.resolve("movies", "movies.txt");
+
+  return fs.readFile(FILE_PATH, { encoding: "utf8" });
+}
